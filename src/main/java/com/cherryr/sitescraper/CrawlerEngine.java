b365e6abb8d533e2c3d59eb7bc6d1d02a996de32
cultivatedvariety <rob.cherry@gmail.com>
2016-06-29 06:10:33 +0100

    initial project commit

---
+++ src/main/java/com/cherryr/sitescraper/CrawlerEngine.java
@@ -0,0 +1,7 @@
+package com.cherryr.sitescraper;
+
+/**
+ * Main engine for crawling a set of URLs. Makes use of a spider to produce items and urls
+ */
+public class CrawlerEngine {
+}
---
+++ src/main/java/com/cherryr/sitescraper/Main.java
@@ -0,0 +1,8 @@
+package com.cherryr.sitescraper;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
